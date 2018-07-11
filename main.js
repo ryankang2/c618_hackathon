6f39ab33849dd50a753b0a70bc91e44039d34b83
Ryan Kang <shk130@ucsd.edu>
2018-07-11 09:37:23 -0700

    need to add jQuery

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready(initializeApp);
 var gameBoard = null;       //8x8 gameboard that's dynamically created
 var boardSize = { rows: 8, cols: 8};
 
+
 class Piece{
     //color -> which player, isKing -> is the piece a king piece or not
     //isAlive -> true if on board, false if not
@@ -18,14 +19,14 @@ class Piece{
     //function to "eat" another piece, call this in the move function
     jumpOver(){
         
-    }
-
-
-    
+    }    
 }
 
-function initializeApp(){
 
+
+function initializeApp(){
+    applyClickHandlers();
+    createBoard();
 }
 
 function applyClickHandlers(){
@@ -34,6 +35,7 @@ function applyClickHandlers(){
 
 //function to dynamically 
 function createBoard(){
+    console.log("here");
     for(var row = 0; row < boardSize.rows; row++){
         var rowDiv = $("<div>", {class: "row"});
         for(var col = 0; col < boardSize.cols; col++){
