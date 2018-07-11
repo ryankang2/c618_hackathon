dcd91219bdf18812bd34c51bf043ec82f91dd461
Ryan Kang <shk130@ucsd.edu>
2018-07-11 11:17:07 -0700

    objects - Ryan

--- main.js
+++ main.js
@@ -1,60 +1,95 @@
 $(document).ready(initializeApp);
 
-var gameBoard = null;       //8x8 gameboard that's dynamically created
-var boardSize = { rows: 8, cols: 8};
+var gameBoard = null; //8x8 gameboard that's dynamically created
+var boardSize = {
+    rows: 8,
+    cols: 8
+};
 
+function initializeApp() {
+    // applyClickHandlers();
+    // var testPiece = new Piece();
+    createBoard();
+    applyClickHandlers();
+}
 
-class Piece{
+class Piece {
     //color -> which player, isKing -> is the piece a king piece or not
     //isAlive -> true if on board, false if not
-    constructor(color, isKing, isAlive){
-        this.color = color;
+    constructor(shape, isKing, isAlive) {
+        if(shape === "triangle"){
+            this.image = "img/triangle.png";
+            this.shape = shape;
+        }
+        if(shape === "cicle"){
+            this.image = "img/circle.png";
+            this.shape = shape;
+        }
+        this.shape = shape;
         this.isKing = isKing;
     }
-    //function to jump onto a diagonal square
-    possibleMoves() { //shows possibles choices
-    // var xLocation = $(event.target.parentElement).attr('x');
-    // var yLocation =  $(event.target.parentElement).attr('y');
-    // console.log(xLocation, "+" , yLocation);
-    console.log('here');
+    //shows possibles choices
+    possibleMoves(){
+        // var xLocation = $(event.target.parentElement).attr('x');
+        // var yLocation =  $(event.target.parentElement).attr('y');
+        // console.log(xLocation, "+" , yLocation);
+        console.log('here');
     }
-    move(){ //physically moves the pieces 
-        if (this.color = "black") { 
-            $('')
+
+    //function to jump onto a diagonal square
+    move() { //physically moves the pieces 
+        if (this.color = "black") {
+
         }
+    }
+    applyClickHandlers() {
+        $(".square").click(this.possibleMoves);
     }
 
     //function to "eat" another piece, call this in the move function
     jumpOver(){
-        
-    }    
+
+    }
 }
 
-
-
-function initializeApp(){
-    applyClickHandlers();
-    createBoard();
-}
-
-function applyClickHandlers(){
-  $('.square').click(possibleMoves);
-
+function applyClickHandlers() {
+    $('.square').click(this.possibleMoves);
 }
 
 //function to dynamically 
-function createBoard(){
+function createBoard() {
     gameBoard = $(".gameBoard");
-    for(var row = 0; row < boardSize.rows; row++){
-        var rowDiv = $("<div>", {class: "row"});
-        for(var col = 0; col < boardSize.cols; col++){
-            var squareDiv = $("<div>", {class: "square", x: row, y: col});
-            if((row+col)%2 === 0){
+    for (var row = 0; row < boardSize.rows; row++) {
+        var rowDiv = $("<div>", {
+            class: "row"
+        });
+        for (var col = 0; col < boardSize.cols; col++) {
+            var squareDiv = $("<div>", {
+                class: "square",
+                x: row,
+                y: col
+            });
+            //check if board square is blue
+            if ((row + col) % 2 === 0) {
                 squareDiv.addClass("light");
-                squareDiv.addClass("blackPiece");
-            }
-            else{
+            } 
+            //check if board square is black
+            else {
                 squareDiv.addClass("dark");
+                if(row >= 5){
+                    var playerOnePiece = new Piece("triangle");
+                    console.log("playerOne: ", playerOnePiece);
+                    // var pieceDiv = $("<div>", {
+                    //     css: "background-image: url(" + playerOnePiece.image + ")"
+                    // });
+                    var pieceDiv = $("<img>").attr("src", playerOnePiece.image);
+                    squareDiv.append(pieceDiv);
+                }
+                if(row <= 2){
+                    var playerOnePiece = new Piece("triangle");
+                    squareDiv.addClass("circlePiece");
+
+                }
             }
             rowDiv.append(squareDiv);
         }
