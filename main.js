2120108482c358cbd39f4671b61c1e2bc50e7ecf
Ryan Kang <shk130@ucsd.edu>
2018-07-11 09:24:32 -0700

    added js and squares in css

---
+++ main.js
@@ -0,0 +1,50 @@
+$(document).ready(initializeApp);
+
+var gameBoard = null;       //8x8 gameboard that's dynamically created
+var boardSize = { rows: 8, cols: 8};
+
+class Piece{
+    //color -> which player, isKing -> is the piece a king piece or not
+    //isAlive -> true if on board, false if not
+    constructor(color, isKing, isAlive){
+        this.color = color;
+        this.isKing = isKing;
+    }
+    //function to jump onto a diagonal square
+    move(){
+
+    }
+
+    //function to "eat" another piece, call this in the move function
+    jumpOver(){
+        
+    }
+    
+
+    
+}
+
+function initializeApp(){
+
+}
+
+function applyClickHandlers(){
+
+}
+
+function createBoard(){
+    for(var row = 0; row < boardSize.rows; row++){
+        var rowDiv = $("<div>", {class: "row"});
+        for(var col = 0; col < boardSize.cols; col++){
+            var squareDiv = $("div>", {class: "square"});
+            if((row+col)%2 === 0){
+                squareDiv.addClass("light");
+            }
+            else{
+                squareDiv.addClass("dark");
+            }
+            rowDiv.append(squareDiv);
+        }
+        gameBoard.append(rowDiv);
+    }
+}
